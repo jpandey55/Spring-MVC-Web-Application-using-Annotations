62caa48e45eb819dae3ee0cff2ec211e29414726
Jitendra Pandey <jpandey@buffalo.edu>
2016-04-13 11:50:26 -0400

    Created XML file

--- AOP_XmlApproach_MethodBeforeAdvice/src/com/me/services/LogBeforeService.java
+++ AOP_XmlApproach_MethodBeforeAdvice/src/com/me/services/LogBeforeService.java
@@ -12,9 +12,7 @@ public class LogBeforeService implements MethodBeforeAdvice {
 
 	@Override
 	public void before(Method m, Object[] param, Object o) throws Throwable {
-		System.out.println(m.getName());
-		System.out.println(o.toString());
-		System.out.println(param);
+	
 		Log l = LogFactory.getLog(Bank.class);
 		l.info("Executing Service before Deposite Method");
 	}
--- AOP_XmlApproach_MethodBeforeAdvice/src/com/me/test/Test.java
+++ AOP_XmlApproach_MethodBeforeAdvice/src/com/me/test/Test.java
@@ -1,25 +1,14 @@
 package com.me.test;
 
-import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.me.business.Bank;
-import com.me.services.LogBeforeService;
 
 public class Test {
 	public static void main(String[] args) {
-		//Create Target
-		Bank b = new Bank();
-		
-		//Create Advice
-		LogBeforeService lbs = new LogBeforeService();
-		
-		//Add Target and Advice to Proxy
-		ProxyFactoryBean pfb = new ProxyFactoryBean();
-		pfb.setTarget(b);
-		pfb.addAdvice(lbs);
-		
-		//Get generated Proxy
-		Bank bProxy = (Bank)pfb.getObject();
+		ApplicationContext ap = new ClassPathXmlApplicationContext("resources/spring.xml"); 
+		Bank bProxy = (Bank)ap.getBean("pfb");
 		int TotalAmount = bProxy.deposite("BOA123", 5000);
 		System.out.println(TotalAmount);
 	}
