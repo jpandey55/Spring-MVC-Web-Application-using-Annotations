0fb6418f2d3c0b1ebef21284e55876c858195107
Jitendra Pandey <jpandey@buffalo.edu>
2016-04-13 11:57:54 -0400

    Closing the container. Ready to execute!

--- AOP_XmlApproach_MethodBeforeAdvice/src/com/me/test/Test.java
+++ AOP_XmlApproach_MethodBeforeAdvice/src/com/me/test/Test.java
@@ -1,16 +1,17 @@
 package com.me.test;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.me.business.Bank;
 
 public class Test {
 	public static void main(String[] args) {
-		ApplicationContext ap = new ClassPathXmlApplicationContext("resources/spring.xml"); 
+		ConfigurableApplicationContext ap = new ClassPathXmlApplicationContext("resources/spring.xml"); 
 		Bank bProxy = (Bank)ap.getBean("pfb");
 		int TotalAmount = bProxy.deposite("BOA123", 5000);
 		System.out.println(TotalAmount);
+		ap.close();
 	}
 
 }
